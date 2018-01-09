3ee50f51e31d19df48f33edbfb830a3dfbca4143
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2018-01-09 23:58:32 +0900

    Add test cases

--- src/test/java/net/arnx/jef4j/Jef4jCharsetProviderTest.java
+++ src/test/java/net/arnx/jef4j/Jef4jCharsetProviderTest.java
@@ -2,6 +2,9 @@ package net.arnx.jef4j;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.charset.CharacterCodingException;
@@ -9,7 +12,10 @@ import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
 import java.nio.charset.CharsetEncoder;
 import java.nio.charset.CodingErrorAction;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.junit.Test;
 
@@ -41,6 +47,162 @@ public class Jef4jCharsetProviderTest {
 						(byte)0x77, (byte)0xA9
 				}, 
 				"🄀".getBytes(Charset.forName("x-Fujitsu-JEF")));
+	}
+	
+	@Test
+	public void testFujitsuEbcdicEncoder() throws IOException {
+		Map<String, String> expected = new TreeMap<>();
+		
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(
+				getClass().getResourceAsStream("/ebcdic_mapping.txt"), 
+				StandardCharsets.UTF_8))) {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				String[] parts = line.split(" ");
+				expected.put(parts[0], parts[1]);
+			}
+		}
+		
+		Map<String, String> actual = new TreeMap<>();
+		
+		CharsetEncoder ce = Charset.forName("x-Fujitsu-EBCDIC")
+				.newEncoder()
+				.onUnmappableCharacter(CodingErrorAction.REPORT)
+				.onMalformedInput(CodingErrorAction.REPORT);
+		CharBuffer cb = CharBuffer.allocate(2);
+	
+		for (int cp = 0; cp <= Character.MAX_CODE_POINT; cp++) {
+			cb.clear();
+			cb.put(Character.toChars(cp));
+			cb.flip();
+			try {
+				ByteBuffer bb = ce.encode(cb);
+				cb.flip();
+				
+				actual.put(hex(cb), hex(bb));
+			} catch (CharacterCodingException e) {
+			}
+			
+			ce.reset();
+		}
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testFujitsuEbcdikEncoder() throws IOException {
+		Map<String, String> expected = new TreeMap<>();
+		
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(
+				getClass().getResourceAsStream("/ebcdik_mapping.txt"), 
+				StandardCharsets.UTF_8))) {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				String[] parts = line.split(" ");
+				expected.put(parts[0], parts[1]);
+			}
+		}
+		
+		Map<String, String> actual = new TreeMap<>();
+		
+		CharsetEncoder ce = Charset.forName("x-Fujitsu-EBCDIK")
+				.newEncoder()
+				.onUnmappableCharacter(CodingErrorAction.REPORT)
+				.onMalformedInput(CodingErrorAction.REPORT);
+		CharBuffer cb = CharBuffer.allocate(2);
+	
+		for (int cp = 0; cp <= Character.MAX_CODE_POINT; cp++) {
+			cb.clear();
+			cb.put(Character.toChars(cp));
+			cb.flip();
+			try {
+				ByteBuffer bb = ce.encode(cb);
+				cb.flip();
+				
+				actual.put(hex(cb), hex(bb));
+			} catch (CharacterCodingException e) {
+			}
+			
+			ce.reset();
+		}
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testFujitsuAsciiEncoder() throws IOException {
+		Map<String, String> expected = new TreeMap<>();
+		
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(
+				getClass().getResourceAsStream("/ascii_mapping.txt"), 
+				StandardCharsets.UTF_8))) {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				String[] parts = line.split(" ");
+				expected.put(parts[0], parts[1]);
+			}
+		}
+		
+		Map<String, String> actual = new TreeMap<>();
+		
+		CharsetEncoder ce = Charset.forName("x-Fujitsu-ASCII")
+				.newEncoder()
+				.onUnmappableCharacter(CodingErrorAction.REPORT)
+				.onMalformedInput(CodingErrorAction.REPORT);
+		CharBuffer cb = CharBuffer.allocate(2);
+	
+		for (int cp = 0; cp <= Character.MAX_CODE_POINT; cp++) {
+			cb.clear();
+			cb.put(Character.toChars(cp));
+			cb.flip();
+			try {
+				ByteBuffer bb = ce.encode(cb);
+				cb.flip();
+				
+				actual.put(hex(cb), hex(bb));
+			} catch (CharacterCodingException e) {
+			}
+			
+			ce.reset();
+		}
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testFujitsuJefEncoder() throws IOException {
+		Map<String, String> expected = new TreeMap<>();
+		
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(
+				getClass().getResourceAsStream("/jef_mapping.txt"), 
+				StandardCharsets.UTF_8))) {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				String[] parts = line.split(" ");
+				expected.put(parts[0], parts[1]);
+			}
+		}
+		
+		Map<String, String> actual = new TreeMap<>();
+		
+		CharsetEncoder ce = Charset.forName("x-Fujitsu-JEF")
+				.newEncoder()
+				.onUnmappableCharacter(CodingErrorAction.REPORT)
+				.onMalformedInput(CodingErrorAction.REPORT);
+		CharBuffer cb = CharBuffer.allocate(2);
+	
+		for (int cp = 0; cp <= Character.MAX_CODE_POINT; cp++) {
+			cb.clear();
+			cb.put(Character.toChars(cp));
+			cb.flip();
+			try {
+				ByteBuffer bb = ce.encode(cb);
+				cb.flip();
+				
+				actual.put(hex(cb), hex(bb));
+			} catch (CharacterCodingException e) {
+			}
+			
+			ce.reset();
+		}
+		assertEquals(expected, actual);
 	}
 	
 	@Test
