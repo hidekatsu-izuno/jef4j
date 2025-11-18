d6d393976f4526ecc9a2f1dc7fddf29f06dab3c6
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2025-11-19 08:53:04 +0900

    Add keis83 testcases

--- src/test/java/net/arnx/jef4j/HitachiCharsetEncoderTest.java
+++ src/test/java/net/arnx/jef4j/HitachiCharsetEncoderTest.java
@@ -238,11 +238,97 @@ public class HitachiCharsetEncoderTest {
 	}
 
 	@Test
-	public void testHitachiKEIS78HanyoDenshiEncoder() throws IOException {
+	public void testHitachiKEIS83Encoder() throws IOException {
+		Charset KEIS83 = Charset.forName("x-Hitachi-KEIS83");
+		assertEquals("4040A4A240404040B3A44040", hex("aあbc海d".getBytes(KEIS83)));
+
+		Map<String, String> expected = new TreeMap<>();
+		
+		try (JsonParser parser = factory.createParser(new BufferedReader(new InputStreamReader(
+				CharsetIndexGenerator.class.getResourceAsStream("/hitachi_keis83_mapping.json"), 
+				StandardCharsets.UTF_8)))) {
+			while (parser.nextToken() != JsonToken.END_ARRAY) {
+				if (parser.currentToken() == JsonToken.START_OBJECT) {
+					JsonNode node = mapper.readTree(parser);
+					boolean decodeOnly = false;
+					
+					JsonNode optionsNode = node.get("options");
+					if (optionsNode != null && optionsNode.isArray()) {
+						for (JsonNode child : optionsNode) {
+							if ("decode_only".equals(child.asText())) {
+								decodeOnly = true;
+							}
+						}
+					}
+					
+					String unicode = toChars(node, false, false);
+					if (!unicode.equals("FFFD") && !decodeOnly) {
+						expected.put(unicode, node.get("code").asText());
+					}
+				}
+			}
+		}
+		
+		Map<String, String> actual = new TreeMap<>();
+		
+		CharsetEncoder ce = KEIS83
+				.newEncoder()
+				.onUnmappableCharacter(CodingErrorAction.REPORT)
+				.onMalformedInput(CodingErrorAction.REPORT);
+		CharBuffer cb = CharBuffer.allocate(3);
+	
+		for (int cp = 0; cp <= 0x2FFFF; cp++) {
+			if (cp >= 0xE000 && cp <= 0xF8FF) {
+				continue;
+			}
+
+			boolean success = false;
+			cb.clear();
+			cb.put(Character.toChars(cp));
+			cb.flip();
+			try {
+				ByteBuffer bb = ce.encode(cb);
+				cb.flip();
+				
+				actual.put(hex(cb), hex(bb));
+				success = true;
+			} catch (CharacterCodingException e) {
+			}
+
+			ce.reset();
+			if (success) {
+				if (useSp3099(cp)) {
+					cb.clear();
+					cb.put(Character.toChars(cp));
+					cb.put('\u3099');
+					cb.flip();
+					try {
+						ByteBuffer bb = ce.encode(cb);
+						cb.flip();
+						actual.put(hex(cb), hex(bb));
+					} catch (CharacterCodingException e) {
+					}
+					ce.reset();
+				}
+			}
+			
+			ce.reset();
+		}
+		
+		Set<String> keys = new TreeSet<>();
+		keys.addAll(expected.keySet());
+		keys.addAll(actual.keySet());
+		for (String key : keys) {
+			assertEquals(expected.get(key), actual.get(key), key);
+		}
+	}
+
+	@Test
+	public void testHitachiKEIS83HanyoDenshiEncoder() throws IOException {
 		Map<String, String> expected = new TreeMap<>();
 
 		try (JsonParser parser = factory.createParser(new BufferedReader(new InputStreamReader(
-				CharsetIndexGenerator.class.getResourceAsStream("/hitachi_keis78_mapping.json"), 
+				CharsetIndexGenerator.class.getResourceAsStream("/hitachi_keis83_mapping.json"), 
 				StandardCharsets.UTF_8)))) {
 			while (parser.nextToken() != JsonToken.END_ARRAY) {
 				if (parser.currentToken() == JsonToken.START_OBJECT) {
@@ -273,7 +359,7 @@ public class HitachiCharsetEncoderTest {
 		
 		Map<String, String> actual = new TreeMap<>();
 		
-		CharsetEncoder ce = Charset.forName("x-Hitachi-KEIS78-HanyoDenshi")
+		CharsetEncoder ce = Charset.forName("x-Hitachi-KEIS83-HanyoDenshi")
 				.newEncoder()
 				.onUnmappableCharacter(CodingErrorAction.REPORT)
 				.onMalformedInput(CodingErrorAction.REPORT);
@@ -444,10 +530,149 @@ public class HitachiCharsetEncoderTest {
 	}
 
 	@Test
+	public void testHitachiKEIS83AdobeJapan1Encoder() throws IOException {
+		Map<String, String> expected = new TreeMap<>();
+
+		try (JsonParser parser = factory.createParser(new BufferedReader(new InputStreamReader(
+				CharsetIndexGenerator.class.getResourceAsStream("/hitachi_keis83_mapping.json"), 
+				StandardCharsets.UTF_8)))) {
+			while (parser.nextToken() != JsonToken.END_ARRAY) {
+				if (parser.currentToken() == JsonToken.START_OBJECT) {
+					JsonNode node = mapper.readTree(parser);
+					boolean decodeOnly = false;
+
+					JsonNode optionsNode = node.get("options");
+					if (optionsNode != null && optionsNode.isArray()) {
+						for (JsonNode child : optionsNode) {
+							if ("decode_only".equals(child.asText())) {
+								decodeOnly = true;
+							}
+						}
+					}
+
+					String unicode = toChars(node, false, false);
+					if (!unicode.equals("FFFD") && !decodeOnly) {
+						expected.put(unicode, node.get("code").asText());
+					}
+
+					unicode = toChars(node, false, true);
+					if (!unicode.equals("FFFD") && !decodeOnly) {
+						expected.put(unicode, node.get("code").asText());
+					}
+				}
+			}
+		}
+		
+		Map<String, String> actual = new TreeMap<>();
+		
+		CharsetEncoder ce = Charset.forName("x-Hitachi-KEIS83-AdobeJapan1")
+				.newEncoder()
+				.onUnmappableCharacter(CodingErrorAction.REPORT)
+				.onMalformedInput(CodingErrorAction.REPORT);
+		CharBuffer cb = CharBuffer.allocate(4);
+	
+		for (int cp = 0; cp <= 0x2FFFF; cp++) {
+			if (cp >= 0xE000 && cp <= 0xF8FF) {
+				continue;
+			}
+
+			boolean success = false;
+			cb.clear();
+			cb.put(Character.toChars(cp));
+			cb.flip();
+			try {
+				ByteBuffer bb = ce.encode(cb);
+				cb.flip();
+				
+				actual.put(hex(cb), hex(bb));
+				success = true;
+			} catch (CharacterCodingException e) {
+			}
+			
+			ce.reset();
+
+			if (success) {
+				if (useSp3099(cp)) {
+					cb.clear();
+					cb.put(Character.toChars(cp));
+					cb.put('\u3099');
+					cb.flip();
+					try {
+						ByteBuffer bb = ce.encode(cb);
+						cb.flip();
+						actual.put(hex(cb), hex(bb));
+					} catch (CharacterCodingException e) {
+					}
+					ce.reset();
+				} else {
+					for (int ivs = 0x0; ivs <= 0xF; ivs++) {
+						StringBuilder sb = new StringBuilder();
+						sb.appendCodePoint(cp);
+						sb.appendCodePoint(0xE0100 + ivs);
+						cb.clear();
+						cb.append(sb);
+						cb.flip();
+						try {
+							ByteBuffer bb = ce.encode(cb);
+							cb.flip();
+							actual.put(hex(cb), hex(bb));
+						} catch (CharacterCodingException e) {
+						}
+						ce.reset();
+					}	
+				}
+			}
+		}
+		
+		Set<String> keys = new TreeSet<>();
+		keys.addAll(expected.keySet());
+		keys.addAll(actual.keySet());
+		for (String key : keys) {
+			assertEquals(expected.get(key), actual.get(key), "key = " + key);
+		}
+	}
+
+	@Test
 	public void testHitachiKEIS78UserDefinedSpaceEncoder() throws IOException {
 		Map<String, String> actual = new TreeMap<>();
 
 		CharsetEncoder ce = Charset.forName("x-Hitachi-KEIS78")
+				.newEncoder()
+				.onUnmappableCharacter(CodingErrorAction.REPORT)
+				.onMalformedInput(CodingErrorAction.REPORT);
+		CharBuffer cb = CharBuffer.allocate(2);
+
+		for (char c = '\uE000'; c <= '\uF8FF'; c++) {
+			cb.clear();
+			cb.put(c);
+			cb.flip();
+			try {
+				ByteBuffer bb = ce.encode(cb);
+				cb.flip();
+				
+				actual.put(hex(cb), hex(bb));
+			} catch (CharacterCodingException e) {
+			}
+			
+			ce.reset();
+		}
+
+		assertEquals("81A1", actual.get("E000"));
+		assertEquals("81FE", actual.get("E05D"));
+		assertEquals("96A1", actual.get("E7B6"));
+		assertEquals("96FE", actual.get("E813"));
+		assertEquals("A0A1", actual.get("EB62"));
+		assertEquals("A0FE", actual.get("EBBF"));
+		assertNull(actual.get("EBC0"));
+		assertNull(actual.get("F8FF"));
+	}
+
+
+	@Test
+	public void testHitachiKEIS83UserDefinedSpaceEncoder() throws IOException {
+		Map<String, String> actual = new TreeMap<>();
+
+		CharsetEncoder ce = Charset.forName("x-Hitachi-KEIS83")
 				.newEncoder()
 				.onUnmappableCharacter(CodingErrorAction.REPORT)
 				.onMalformedInput(CodingErrorAction.REPORT);
@@ -485,9 +710,21 @@ public class HitachiCharsetEncoderTest {
 	}
 
 	@Test
+	public void testHitachiKEIS83EbcdicEncoder() throws IOException {
+		Charset KEIS78_EBCDIC = Charset.forName("x-Hitachi-KEIS83-EBCDIC");
+		assertEquals("810A42A4A20A41820A42B3A40A4183", hex("aあb海c".getBytes(KEIS78_EBCDIC)));
+	}
+
+	@Test
 	public void testHitachiKEIS78EbcdikEncoder() throws IOException {
 		Charset KEIS78_EBCDIK = Charset.forName("x-Hitachi-KEIS78-EBCDIK");
 		assertEquals("810A42A4A20A41820A42B3A40A4183", hex("ｱあｲ海ｳ".getBytes(KEIS78_EBCDIK)));
+	}
+
+	@Test
+	public void testHitachiKEIS83EbcdikEncoder() throws IOException {
+		Charset KEIS83_EBCDIK = Charset.forName("x-Hitachi-KEIS83-EBCDIK");
+		assertEquals("810A42A4A20A41820A42B3A40A4183", hex("ｱあｲ海ｳ".getBytes(KEIS83_EBCDIK)));
 	}
 		
 	private static String toChars(JsonNode node, boolean useHanyoDenshi, boolean useAdobeJapan1) {
